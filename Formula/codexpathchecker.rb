199d97f96a89a8fb69c8774c1dae6f1a03a3c3a7
adamzafir <adam_zafir_mohammed_sudhir+github@s2024.ssts.edu.sg>
2026-03-17 13:20:30 +0800

    Add Homebrew release checksum

--- Formula/codexpathchecker.rb
+++ Formula/codexpathchecker.rb
@@ -2,7 +2,7 @@ class Codexpathchecker < Formula
   desc "Warn before running codex in the wrong working directory"
   homepage "https://github.com/adamzafir/codexpathchecker"
   url "https://github.com/adamzafir/codexpathchecker/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "REPLACE_SHA256"
+  sha256 "fb8e222af8ead9d0d3ddbc8aeebf0790ba8b0fcc28dfe0648c215864a7695078"
   license "MIT"
   head "https://github.com/adamzafir/codexpathchecker.git", branch: "main"
 
